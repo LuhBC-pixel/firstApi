bfca6117b9334f3aab82d444b52fa840ef64ebca
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-11-27 18:13:19 -0300

    Captura o body das requests

--- src/index.js
+++ src/index.js
@@ -24,6 +24,11 @@ const server = http.createServer((request, response) => {
     if (route) {
         request.query = parseUrl.query;
         request.params = { id };
+
+        response.send = (statusCode, body) => {
+            response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify(body));
+        }
         
         route.handler(request, response);
     } else {
