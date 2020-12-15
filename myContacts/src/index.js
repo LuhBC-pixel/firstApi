85eec79f816ecf3a992a2f01f731a1ffc801d49b
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-12-15 20:45:08 -0300

    a

--- myContacts/src/index.js
+++ myContacts/src/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello World!');
+  response.send('Hello World!');
 });
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
